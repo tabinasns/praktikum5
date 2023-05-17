2b82a5c9932dd04016b7ad83a83ec1ce7cf1df2d
tabina <111294387+tabinasns@users.noreply.github.com>
2023-05-17 22:03:41 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\EmployeeController;
-use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
